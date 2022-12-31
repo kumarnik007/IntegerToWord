2e26b387abe16ee68fd6f5d1ed18df3818e6ca46
Nikhil Kumar <nkhlneil@gmail.com>
2022-12-31 07:22:25 +0100

    Minor changes

--- integerToWordConversion.go
+++ integerToWordConversion.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 // Constants for Conversion
 const EMPTY string = ""
 
@@ -19,9 +21,9 @@ func convert2digit(inputNumber int, inputSuffix string) string {
 
 	if inputNumber > 19 {
 		return Tenths[inputNumber/10] + LessThanTwenty[inputNumber%10] + inputSuffix
-	} else {
-		return LessThanTwenty[inputNumber] + inputSuffix
 	}
+
+	return LessThanTwenty[inputNumber] + inputSuffix
 }
 
 func convert3digit(inputNumber int, inputSuffix string) string {
@@ -33,35 +35,33 @@ func convert3digit(inputNumber int, inputSuffix string) string {
 		if ((inputNumber % 100) > 9) &&
 			((inputNumber % 100) < 20) {
 			return (LessThanTwenty[inputNumber/100] + "Hundred " + convert2digit((inputNumber%100), EMPTY) + inputSuffix)
-		} else {
-			return (LessThanTwenty[inputNumber/100] + "Hundred " + Tenths[(inputNumber/10)%10] + LessThanTwenty[inputNumber%10] + inputSuffix)
 		}
-	} else {
-		return convert2digit(inputNumber, inputSuffix)
+
+		return (LessThanTwenty[inputNumber/100] + "Hundred " + Tenths[(inputNumber/10)%10] + LessThanTwenty[inputNumber%10] + inputSuffix)
 	}
+
+	return convert2digit(inputNumber, inputSuffix)
 }
 
 func NumberToWords(inputNumber int) (string, error) {
-	var resultWord string
-	var errorResponse error
-
 	if inputNumber == 0 {
-		resultWord = "Zero "
-	} else if 0 > inputNumber {
-		resultWord = EMPTY
-		errorResponse = NewHTTPError(nil, 400, "Bad Input - Negative Integer is not accepted")
-	} else if 999999999 < inputNumber {
-		resultWord = EMPTY
-		errorResponse = NewHTTPError(nil, 400, "Bad Input - Value greater than 999999999 cannot be processed")
-	} else {
-		resultWord = convert2digit((inputNumber % 100), "")
-
-		resultWord = convert3digit(((inputNumber/100)%10), "Hundred ") + resultWord
-
-		resultWord = convert3digit(((inputNumber/1000)%1000), "Thousand ") + resultWord
-
-		resultWord = convert3digit(((inputNumber/1000000)%1000), "Million ") + resultWord
+		return "Zero", nil
 	}
 
-	return resultWord, errorResponse
+	if 0 > inputNumber {
+		return EMPTY, NewHTTPError(nil, 400, "Bad Input - Negative Integer is not accepted")
+	}
+
+	if 999999999 < inputNumber {
+		return EMPTY, NewHTTPError(nil, 400, "Bad Input - Value greater than 999999999 cannot be processed")
+	}
+
+	var resultWord string = EMPTY
+
+	resultWord = convert2digit((inputNumber % 100), "")
+	resultWord = convert3digit(((inputNumber/100)%10), "Hundred ") + resultWord
+	resultWord = convert3digit(((inputNumber/1000)%1000), "Thousand ") + resultWord
+	resultWord = convert3digit(((inputNumber/1000000)%1000), "Million ") + resultWord
+
+	return strings.TrimRight(resultWord, " "), nil
 }
